bfeeb6459cfa9a3c9ef51a8c219d5d27763c79e1
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-07-13 16:06:05 +0300

    variables and data types

---
+++ lessons_2/lessons_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lessons_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            byte age, a = 0, b = 0;
+            int res;
+
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter age: ");
+            age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("My name " + name + " i am " + age + " years old");
+
+            Console.WriteLine("Enter a: ");
+            a = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+            b = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Result: " + (a+b).ToString());
+            Console.Read();
+        }
+    }
+}
