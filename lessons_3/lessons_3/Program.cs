93b0bdef75dbdb8d1f832a5fe06a58f022a4a9ba
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-07-14 14:21:09 +0300

    conditional statements and operations

---
+++ lessons_3/lessons_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lessons_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num;
+            bool hasCar = true;
+            string str, name;
+
+            Console.WriteLine("Enter one number");
+            num = Convert.ToInt16(Console.ReadLine());
+
+            if (num == 10)
+                Console.WriteLine("the number is exactly 10");
+
+            else if (num > 50)
+                Console.WriteLine("the number is greater 50");
+            else
+                Console.WriteLine("the number unknow");
+
+            str = num < 100 && hasCar && num==54 ? "Yes" : "No";
+            Console.WriteLine(str);
+
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+
+            switch (name)
+            {
+                case "Vadim":
+                    Console.WriteLine("Registration renewed");
+                        break;
+                case "Nikolay":
+                    Console.WriteLine("registration denied");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
