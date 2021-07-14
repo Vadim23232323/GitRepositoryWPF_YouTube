5db14e04194ae8ad1be377d742d5a784344818d1
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-07-14 16:29:45 +0300

    Array

---
+++ lessons_4/lessons_4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lessons_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] mas = new byte[5];
+
+            mas[0] = 27;
+
+            Console.WriteLine(mas[0]);
+
+            string[] students = new string[] { "Tatia", "Sergey", "Maksim", "Egor", "Vadim" };
+
+            Console.WriteLine(students[0]);
+
+            int[] numbers = { 5, 2, 500, 76, 80 };
+
+            int[,] people = { { 5, 8, 6 }, { 5, 8, 6 }, { 5, 8, 6 } };
+
+            Console.WriteLine(people[1, 1]);
+
+            people[1, 1] = 50;
+
+            Console.WriteLine(people[1, 1]);
+
+            Console.WriteLine("array length" + people.Length);
+
+            Array.Clear(people, 2, people.Length);
+
+            int[] copyPeople = new int[people.Length];
+            people.CopyTo(copyPeople, 0);
+
+            Console.WriteLine(copyPeople[1]);
+
+            List<short> digits = new List<short>() { 1, 0, 93, 75 };
+            digits.Add(5);
+            digits.AddRange(new short[] { 7, 8, 45 });
+            digits.Insert(1, 178);
+            digits.Remove(0);
+            digits.RemoveAt(2);
+            digits.Sort();
+            Console.WriteLine(digits[2]);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
