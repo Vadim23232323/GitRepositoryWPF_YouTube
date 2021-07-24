a124a0bddd13751a8a6853580e53e953ae108345
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-07-24 12:42:45 +0300

    Creating Classes and Methods

---
+++ lessons_8/lessons_8/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessons_8
+{
+    public class Admin
+    {
+        public string role = null;
+    }
+}
