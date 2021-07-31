1aa9d4ade620a71024d394c3bec45b709b2cefe2
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-07-31 09:55:12 +0300

    Thread

---
+++ lessons_15/lessons_15/Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lessons_15
+{
+    public class Numbers { 
+    public int x, y;
+    public Numbers(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void printSome()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("My stream - " + x + ", " + y);
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
---
+++ lessons_15/lessons_15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace lessons_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Thread tr = Thread.CurrentThread;
+
+            //Console.WriteLine(tr.Name);
+            //tr.Name = "Main stream";
+            //Console.WriteLine(tr.Name);
+            //Console.WriteLine(tr.Priority);
+            //Console.WriteLine(tr.ThreadState);
+            //Console.WriteLine(tr.IsAlive);
+            //Console.WriteLine(Thread.GetDomain().FriendlyName);
+
+
+            //ThreadStart ts = new ThreadStart(printSome);
+            //Thread myTr = new Thread(ts);
+            Numbers num = new Numbers(5, 6);
+            Thread myTr = new Thread(new ThreadStart(num.printSome));
+            myTr.Start(num);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("main strem");
+                Thread.Sleep(300);
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
